0d1d9324696901d7c400caf15334d175412b1c06
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-12-04 15:46:20 +0000

    updated to temp code

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST': 
-        # get data from input fields
-        st_var_mean = request.form.get("st_var_mean")
-        perc_time_abnormal_st_var = request.form.get("perc_time_abnormal_st_var")
-        abnormal_st_var = request.form.get("abnormal_st_var")
-        hist_mean = request.form.get("hist_mean")
-        hist_var = request.form.get("hist_var")
+def read_file(file_path):
+    disease_data = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split('\t')
+            if len(parts) == 2:
+                disease_name = parts[0]
+                word_count_pairs = parts[1].split(',')
+                words = [pair.split(':')[1] for pair in word_count_pairs if len(pair.split(':')) == 2]
+                disease_data[disease_name] = words
 
-        # Run the model with the input fields (for now just return dummy %)
+    return disease_data
 
-        print("inputs: ", st_var_mean, perc_time_abnormal_st_var, abnormal_st_var, hist_mean, hist_var)
-
-        result = 42
-
-        return render_template('result.html', result=result)
-    
-    return render_template('home.html')
+@app.route('/')
+def index():
+    file_path = 'merged-output.txt'  # Replace with the actual file path
+    disease_data = read_file(file_path)
+    return render_template('index.html', disease_data=disease_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
