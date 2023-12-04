180ac2bd04b42acec8be1e9a0e92f92bf10601a4
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2023-12-04 18:02:16 +0000

    Update app.py
    formatting and commenting

--- app.py
+++ app.py
@@ -2,24 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def read_file(file_path):
+
+@app.route('/')
+def home():
+    # A simple home page controller. Reads a file and loads a jinja template.
+    # Could do with much more error handling but that is outside the scope of assignment.
+    path = 'merged-output.txt' 
+    disease_data = read_output_file(path)
+
+    return render_template('index.html', disease_data=disease_data)
+
+
+# Reads the map reducd output file.
+# Creates a dictionary of disease_name : [ topword1, topword2, ... topwordN ]
+def read_output_file(path):
+
     disease_data = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            parts = line.strip().split('\t')
-            if len(parts) == 2:
-                disease_name = parts[0]
-                word_count_pairs = parts[1].split(',')
-                words = [pair.split(':')[1] for pair in word_count_pairs if len(pair.split(':')) == 2]
+
+    with open(path, 'r') as f:
+
+        for line in f:
+            tokens = line.strip().split('\t')
+            if len(tokens) == 2:
+                # get disease name from start of line
+                disease_name = tokens[0]
+                # get the top words associated with the disease.
+                top_words = tokens[1].split(',')
+                # remove the word count from [count:word]
+                words = [pair.split(':')[1] for pair in top_words if len(pair.split(':')) == 2]
+                # Put in dictionary
                 disease_data[disease_name] = words
 
     return disease_data
 
-@app.route('/')
-def index():
-    file_path = 'merged-output.txt'  # Replace with the actual file path
-    disease_data = read_file(file_path)
-    return render_template('index.html', disease_data=disease_data)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
